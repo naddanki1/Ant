c6ca41b0316e73e4e44133e1c993ded80b4817f3
rvegesna <rvegesna@localhost>
2016-05-18 03:45:14 -0700

    changed the files

---
+++ src/HiddenField2.java
@@ -0,0 +1,36 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class HiddenField2 extends HttpServlet {
+        private static final long serialVersionUID = 1L;
+
+
+    public HiddenField2() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+
+        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+                response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        //Getting the value from the hidden field
+        String n=request.getParameter("uname");
+        out.print("Hello "+n);
+
+        out.close();
+        }
+
+
+
+        }
+
